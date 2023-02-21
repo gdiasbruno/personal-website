03907b3a1170e273d5039ff134271276cd6a58be
gdiasbruno <gdias.bruno@gmail.com>
2023-02-21 17:58:37 +0100

    Replace image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export function Header (props: IAppProps) {
       </div>
       <div className="sm:grid sm:justify-end sm:w-full hidden" >
     <img
-      src={"/images/hero.jpg"}
+      src={"/images/hero_2.jpg"}
       width="175px"
       className="rounded-full"
     />
